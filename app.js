97ab89ddbe01057ebccadbfa8f6f5e179b29825c
root <root@Sathish.localdomain>
2026-04-09 23:41:56 +0530

    Update message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from DevOps Pipeline 🚀");
+  res.end("Hello from DevOps Pipeline, Successfully launched my first devops project");
 });
 
 server.listen(3000, () => {
