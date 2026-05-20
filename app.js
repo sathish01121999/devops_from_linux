5aba5f8d398c8be7c2191ccfe28784a2c364e683
root <root@Sathish.localdomain>
2026-05-20 22:57:54 +0530

    Second attempt commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from DevOps Pipeline, Successfully launched my first devops project");
+  res.end("<======= Hello from DevOps Pipeline, Successfully launched my second devops project ======>");
 });
 
 server.listen(3000, () => {
