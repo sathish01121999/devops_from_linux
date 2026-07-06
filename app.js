c95d5481ba8a4d4f6870aa4ef975554a84b745d0
root <root@Sathish.localdomain>
2026-07-06 22:29:37 +0530

    Prom & Graf added

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const http = require('http');
+const http = require("http");
+const client = require("prom-client");
 
-const server = http.createServer((req, res) => {
-    res.end("Hiii Buddy, it's me. Any update?");
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics();
+
+const counter = new client.Counter({
+    name: "http_requests_total",
+    help: "Total HTTP Requests"
+});
+
+const server = http.createServer(async (req, res) => {
+
+    if (req.url === "/metrics") {
+        res.setHeader("Content-Type", client.register.contentType);
+        res.end(await client.register.metrics());
+        return;
+    }
+
+    counter.inc();
+
+    res.end("Doing monitoring via Prometheus and Grafana --> Watcha!");
 });
 
 server.listen(3003, () => {
