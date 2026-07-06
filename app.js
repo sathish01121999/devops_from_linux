f3f8e384851671e311bc99fbb5f7432034695610
root <root@Sathish.localdomain>
2026-07-06 22:16:11 +0530

    App port changed to 3003

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
     res.end("Hiii Buddy, it's me. Any update?");
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+server.listen(3003, () => {
+    console.log("Server running on port 3003");
 });
